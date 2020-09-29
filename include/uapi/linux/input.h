1aeba96c7a2404f0d06b595b1cd74943a0b9a137
corsicanu <corsicanu22@gmail.com>
2020-09-29 15:20:16 +0300

    include/input: underp the function to compile with debug disabled

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -105,8 +105,8 @@
 	snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", SECLOG, fmt);	\
 	dev_err(dev, input_log_buf, ## __VA_ARGS__);				\
 })
-#define input_raw_info(mode, dev, fmt, ...) input_info(mode, dev, fmt, ...)
-#define input_log_fix() {}
+#define input_raw_info(mode, dev, fmt, ...) input_info(mode, dev, fmt, ## __VA_ARGS__)
+#define input_log_fix()	{}
 #define input_raw_data_clear() {}
 #endif
 
